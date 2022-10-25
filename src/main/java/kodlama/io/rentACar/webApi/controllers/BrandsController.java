a18588d89ec1ca4a5fc5677c6c9f37d3917e15ad
Kucukdogru <m42erve@hotmail.com>
2022-10-26 00:00:09 +0300

    25 Ekim Spring Boot giriş
    25 Ekim Spring Boot projesi oluşturuldu.

---
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -0,0 +1,33 @@
+package kodlama.io.rentACar.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@RestController // Spring Framework (annotation)
+@RequestMapping("/api/brands")
+public class BrandsController {
+	private BrandService brandService;
+
+	@Autowired
+	public BrandsController(BrandService brandService) {
+		this.brandService = brandService;
+	}
+	
+	@GetMapping("/getall")
+	public List<Brand> getAll(){
+		return brandService.getAll();
+	}
+	
+	@GetMapping("/getbyid")
+	public Brand getById(int id){
+		return brandService.getById(id);
+	}
+	
+}
